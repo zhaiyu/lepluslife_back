23db23ac4b07cf794169bb7314a4e32852e7910c
zhaiyu <42427106@qq.com>
2016-11-23 10:22:50 +0800

    xf-11-23

--- src/main/java/com/jifenke/lepluslive/merchant/service/MerchantRebatePolicyService.java
+++ src/main/java/com/jifenke/lepluslive/merchant/service/MerchantRebatePolicyService.java
@@ -55,7 +55,7 @@ public class MerchantRebatePolicyService {
         }
         if(merchantRebatePolicy.getRebateFlag()==1) {
             rebatePolicy.setUserScoreBScaleB(merchantRebatePolicy.getUserScoreBScaleB());
-            merchantRebatePolicy.setUserScoreBScale(new BigDecimal(0));
+            rebatePolicy.setUserScoreBScale(new BigDecimal(0));
             merchantRebatePolicy.setUserScoreAScale(new BigDecimal(0));
         }
         if(merchantRebatePolicy.getRebateFlag()==2) {
@@ -69,9 +69,5 @@ public class MerchantRebatePolicyService {
         rebatePolicy.setUserScoreBScaleB(new BigDecimal(0));
         rebatePolicy.setUserScoreBScale(new BigDecimal(0));
         rebatePolicy.setUserScoreAScale(new BigDecimal(0));
-        rebatePolicy.setStageOne(0);
-        rebatePolicy.setStageTwo(0);
-        rebatePolicy.setStageThree(0);
-        rebatePolicy.setStageFour(0);
     }
 }
