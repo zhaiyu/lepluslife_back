9f5a9e4778deae6b16a39f45fa77d9183e9bcdf9
unknown <2170951923@qq.com>
2016-12-12 17:12:38 +0800

    hongbao

--- src/main/java/com/jifenke/lepluslive/scoreAAccount/controller/ScoreAAccountController.java
+++ src/main/java/com/jifenke/lepluslive/scoreAAccount/controller/ScoreAAccountController.java
@@ -13,26 +13,19 @@ import com.jifenke.lepluslive.score.service.ScoreADetailService;
 import com.jifenke.lepluslive.scoreAAccount.controller.view.ScoreAAccountDetailViewExcel;
 import com.jifenke.lepluslive.scoreAAccount.controller.view.ScoreAAccountViewExcel;
 import com.jifenke.lepluslive.scoreAAccount.domain.criteria.ScoreAAccountCriteria;
+import com.jifenke.lepluslive.scoreAAccount.domain.criteria.ScoreAAccountDistributionCriteria;
 import com.jifenke.lepluslive.scoreAAccount.domain.entities.ScoreAAccount;
 import com.jifenke.lepluslive.scoreAAccount.service.ScoreAAccountService;
 import com.jifenke.lepluslive.user.domain.entities.LeJiaUser;
-
+import org.apache.commons.collections.map.HashedMap;
 import org.springframework.data.domain.Page;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by lss on 2016/9/12.
@@ -155,7 +148,7 @@ public class ScoreAAccountController {
     Integer issuedScoreA=0;
     Integer commissionIncome=0;
     Integer jfkShare=0;
-    List<Map> scoreAAccountDetailList=scoreAAccountDetailList(scoreDetailCriteria,10000);
+      List<Map> scoreAAccountDetailList = scoreAAccountDetailList(scoreDetailCriteria, 10);
     for(Map scoreAAccountDetailMap:scoreAAccountDetailList){
       if (scoreAAccountDetailMap.get("useScoreA")!=null){
         useScoreA=Integer.parseInt(scoreAAccountDetailMap.get("useScoreA").toString())+useScoreA;
@@ -313,4 +306,232 @@ public class ScoreAAccountController {
     }
     return scoreAAccountDetailList;
   }
+
+    //分布
+    @RequestMapping(value = "/scoreAAccount/distributionList", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    LejiaResult getScoreaDistributionByAjax(@RequestBody ScoreAAccountDistributionCriteria scoreAAccountDistributionCriteria) {
+        List<String> dateList = getDate(scoreAAccountDistributionCriteria);
+        String startDate = dateList.get(0);
+        String endDate = dateList.get(1);
+        if (scoreAAccountDistributionCriteria.getOffset() == null) {
+            scoreAAccountDistributionCriteria.setOffset(1);
+        }
+        List<Object[]> distributionList = scoreAAccountService.findScoreaDistribution(startDate, endDate);
+        List<Object[]> pageList = new ArrayList<Object[]>();
+        int offset = scoreAAccountDistributionCriteria.getOffset();
+        int totalElements = distributionList.size();
+        double totalPages = Math.ceil(totalElements / 5.0);
+        if (totalElements < offset * 5) {
+            pageList = distributionList.subList((offset - 1) * 5, distributionList.size());
+        } else {
+            pageList = distributionList.subList((offset - 1) * 5, offset * 5);
+        }
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("totalPages", totalPages);
+        dataMap.put("totalElements", totalElements);
+        dataMap.put("content", pageList);
+        return LejiaResult.ok(dataMap);
+    }
+    //折线图
+    @RequestMapping(value = "/scoreAAccount/geMyChartByAjax", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    LejiaResult geMyChartByAjax(@RequestBody ScoreAAccountCriteria scoreAAccountCriteria) {
+        String startDate=scoreAAccountCriteria.getStartDate();
+        String endDate=scoreAAccountCriteria.getEndDate();
+        if(startDate!=null&&endDate!=null){
+            startDate =startDate.replaceAll("/","-");
+            endDate = endDate.replaceAll("/","-");
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        List<Date> dateList = new ArrayList<Date>();
+        List<String> dateStrList = new ArrayList<String>();
+        if (startDate==null||endDate==null) {
+            try {
+                Calendar calendarDate = Calendar.getInstance();
+                Date date = new Date();
+                dateList.add(date);
+                calendarDate.setTime(date);
+                for (int i = 1; i < 7; i++) {
+                    calendarDate.set(Calendar.DATE, calendarDate.get(Calendar.DATE) - 1);
+                    dateList.add(sdf.parse(sdf.format(calendarDate.getTime())));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+
+            Calendar cal = Calendar.getInstance();
+            try {
+                Date dBegin = sdf.parse(startDate);
+                Date dEnd = sdf.parse(endDate);
+                dateList = getDatesBetweenTwoDate(dBegin, dEnd);
+                Collections.reverse(dateList);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        for(Date date:dateList){
+            dateStrList.add(sdf.format(date));
+        }
+        if (startDate==null||endDate==null) {
+            endDate=dateStrList.get(0).replaceAll("-","/")+" 23:59:59";
+            startDate=dateStrList.get(dateStrList.size()-1).replaceAll("-","/")+" " + "00:00:00";
+        }else {
+            startDate = scoreAAccountCriteria.getStartDate() + " " + "00:00:00";
+            endDate = scoreAAccountCriteria.getEndDate() + " 23:59:59";
+        }
+        scoreAAccountCriteria.setEndDate(endDate);
+        scoreAAccountCriteria.setStartDate(startDate);
+        Page page = scoreAAccountService.findScoreAAccountByPage(scoreAAccountCriteria, 10000);
+        List<ScoreAAccount> scoreAAccountList=page.getContent();
+        List<Double> useScoreAList=new ArrayList<Double>();
+        List<Double> issuedScoreAList=new ArrayList<Double>();
+        List<Double> commissionIncomeList=new ArrayList<Double>();
+        List<Double> jfkShareList=new ArrayList<Double>();
+        for(ScoreAAccount scoreAAccount :scoreAAccountList){
+            useScoreAList.add( scoreAAccount.getUseScoreA().doubleValue()/100.0);
+            issuedScoreAList.add(scoreAAccount.getIssuedScoreA().doubleValue()/100.0);
+            commissionIncomeList.add(scoreAAccount.getCommissionIncome().doubleValue()/100.0);
+            jfkShareList.add(scoreAAccount.getJfkShare().doubleValue()/100.0);
+        }
+        Map<String,Object> dataMap=new HashMap<String,Object>();
+        dataMap.put("dateStrList",dateStrList);
+        dataMap.put("useScoreAList",useScoreAList);
+        dataMap.put("issuedScoreAList",issuedScoreAList);
+        dataMap.put("jfkShareList",jfkShareList);
+        dataMap.put("commissionIncomeList",commissionIncomeList);
+        return LejiaResult.ok(dataMap);
+    }
+
+
+    //饼图
+    @RequestMapping(value = "/scoreAAccount/getpieChartByAjax", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    LejiaResult getpieChartByAjax(@RequestBody ScoreAAccountDistributionCriteria scoreAAccountDistributionCriteria) {
+        List<String> dateList = getDate(scoreAAccountDistributionCriteria);
+        String startDate = dateList.get(0);
+        String endDate = dateList.get(1);
+        List<Object[]> distributionList = scoreAAccountService.findScoreaDistribution(startDate, endDate);
+        List<String> nameList = new ArrayList<String>();
+        List<Integer> valueList = new ArrayList<Integer>();
+        for (Object[] objects : distributionList) {
+            Integer origin = Integer.valueOf(objects[0].toString());
+            Integer value = Integer.valueOf(objects[1].toString());
+            switch (origin) {
+                case 0:
+                    nameList.add("关注送红包");
+
+                    break;
+                case 1:
+                    nameList.add("线上返还");
+
+                    break;
+                case 2:
+                    nameList.add("线上消费");
+
+                    break;
+                case 3:
+                    nameList.add("线下消费");
+                    break;
+                case 4:
+                    nameList.add("线下返还");
+                    break;
+                case 5:
+                    nameList.add("活动返还");
+                    break;
+                case 6:
+                    nameList.add("运动");
+                    break;
+                case 7:
+                    nameList.add("摇一摇");
+                    break;
+                case 8:
+                    nameList.add("APP分享");
+                    break;
+                case 9:
+                    nameList.add("线下支付完成页注册会员");
+                    break;
+                case 10:
+                    nameList.add("合伙人发福利");
+                    break;
+                case 11:
+                    nameList.add("临时活动");
+                    break;
+                case 13:
+                    nameList.add("充话费发送包");
+                case 14001:
+                    nameList.add("退款");
+                    break;
+            }
+            valueList.add(value);
+        }
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("nameList", nameList);
+        dataMap.put("valueList", valueList);
+        return LejiaResult.ok(dataMap);
+    }
+
+    List<String> getDate(ScoreAAccountDistributionCriteria scoreAAccountDistributionCriteria) {
+        List<String> returnList = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String startDate = null;
+        String endDate = null;
+        if (scoreAAccountDistributionCriteria.getStartDate() == null || scoreAAccountDistributionCriteria.getEndDate() == null) {
+            try {
+                Calendar calendarDate = Calendar.getInstance();
+                Date dNow = new Date();   //当前时间
+                List<Date> dateList = new ArrayList<Date>();
+                for (int i = 0; i < 7; i++) {
+
+                    Date dBefore = new Date();
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(dNow);
+                    calendar.add(Calendar.DAY_OF_MONTH, -i);
+                    dBefore = calendar.getTime();
+
+                    String defaultStartDate = sdf.format(dBefore);
+                    dateList.add(sdf.parse(defaultStartDate));
+
+                }
+                startDate = sdf.format(dateList.get(6)) + " " + "00:00:00";
+                endDate = sdf.format(dateList.get(0)) + " 23:59:59";
+                returnList.add(startDate);
+                returnList.add(endDate);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            startDate = scoreAAccountDistributionCriteria.getStartDate() + " " + "00:00:00";
+            endDate = scoreAAccountDistributionCriteria.getEndDate() + " 23:59:59";
+            returnList.add(startDate);
+            returnList.add(endDate);
+        }
+        return returnList;
+    }
+    public List getDatesBetweenTwoDate(Date beginDate, Date endDate) {
+        List lDate = new ArrayList();
+        lDate.add(beginDate);//把开始时间加入集合
+        Calendar cal = Calendar.getInstance();
+        //使用给定的 Date 设置此 Calendar 的时间
+        cal.setTime(beginDate);
+        boolean bContinue = true;
+        while (bContinue) {
+            //根据日历的规则，为给定的日历字段添加或减去指定的时间量
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+            // 测试此日期是否在指定日期之后
+            if (endDate.after(cal.getTime())) {
+                lDate.add(cal.getTime());
+            } else {
+                break;
+            }
+        }
+        lDate.add(endDate);//把结束时间加入集合
+        return lDate;
+    }
 }
+//
