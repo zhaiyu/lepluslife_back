d011144091ab6b6bbb5d947f5c4e31b872b1f4fa
zhaiyu <42427106@qq.com>
2017-01-03 10:56:01 +0800

    xf-2017-01-03

--- src/main/java/com/jifenke/lepluslive/order/controller/view/FinancialViewExcel.java
+++ src/main/java/com/jifenke/lepluslive/order/controller/view/FinancialViewExcel.java
@@ -105,7 +105,7 @@ public class FinancialViewExcel extends AbstractExcelView {
       excelRow.createCell(16)
           .setCellValue(
               (financialStatistic.getTransferPrice() + financialStatistic.getAppTransfer()
-               + financialStatistic.getPosTransFromTruePay())
+               + (financialStatistic.getPosTransfer()-financialStatistic.getPosTransFromTruePay()))
               / 100.0);
 
     }
